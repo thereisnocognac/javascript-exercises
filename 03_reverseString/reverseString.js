ab9474feb3436a14f60b66e5fe36a3f0d9ecfb21
thereisnocognac <yollovesxyra@gmail.com>
2022-05-02 16:52:50 -0400

    Completed these tasks

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,15 @@
-const reverseString = function() {
+const reverseString = function (string) {
+    let seperateString;
+    let backString;
+    let joinString;
+
+    seperateString = string.split("");
+
+    backString = seperateString.reverse();
+
+    joinString = backString.join("");
+
+    return joinString;
 
 };
 
