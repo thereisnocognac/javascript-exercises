2472b0a5713173fc1a98c7f80dae9bace333a51c
thereisnocognac <yollovesxyra@gmail.com>
2022-05-11 12:23:54 -0400

    Completed Tasks

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,34 @@
-const sumAll = function() {
+const sumAll = function (x, y) {
+    let finalSum;
+    finalSum = 0;
+
+    if (x < y) {
+
+        for (i = x; i <= y - 1; i++) {
+            finalSum = i + finalSum;
+        }
+
+    } else if (x > y) {
+
+        for (i = x; i >= y + 1; i--) {
+            finalSum = i + finalSum;
+        }
+
+    }
+
+    if (x < 0 || y < 0) {
+        return 'ERROR';
+    }
+
+    if (Number.isInteger(x) === false || Number.isInteger(y) === false) {
+        return 'ERROR';
+    }
+
+
+
+    return finalSum + y;
+
+
 
 };
 
